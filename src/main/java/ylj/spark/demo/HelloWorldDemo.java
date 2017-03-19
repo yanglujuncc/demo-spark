f0fd78493afd74575e0262b1b59d8ae6abf165a9
yanglujun <yanglujun@DESKTOP-MI92GJ0>
2017-03-19 21:49:50 +0800

    add

---
+++ src/main/java/ylj/spark/demo/HelloWorldDemo.java
@@ -0,0 +1,66 @@
+package ylj.spark.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+public class HelloWorldDemo {
+	
+	public static void main(String[] args){
+
+        List<String> srcData = Arrays.asList("abc", "a", "b", "c");
+
+        SparkConf conf = new SparkConf().setAppName("Simple Application");
+        conf.setMaster("local[2]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> logData = sc.parallelize(srcData).cache();
+
+ 
+
+        long numAs = logData.filter(new Function<String,Boolean>() {
+
+            /**
+			 * 
+			 */
+			private static final long serialVersionUID = 5381930222098233051L;
+
+			public Boolean call(String s) {
+
+                return s.contains("a");
+
+            }
+
+        }).count();
+
+ 
+
+        long numBs = logData.filter(new Function<String,Boolean>() {
+
+            /**
+			 * 
+			 */
+			private static final long serialVersionUID = 4106701822571019042L;
+
+			public Boolean call(String s) {
+
+                return s.contains("b");
+
+            }
+
+        }).count();
+
+ 
+
+        long count = logData.count();
+
+ 
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: "+ numBs+ ", count = " + count);
+
+    }
+}
---
+++ src/main/java/ylj/spark/demo/SVDExample.java
@@ -0,0 +1,58 @@
+package ylj.spark.demo;
+
+import java.util.LinkedList;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.SingularValueDecomposition;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+
+public class SVDExample {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	public static void tes1t() {
+		
+		
+//		double[][] array = { { 1.12, 2.05, 3.12 }, { 5.56, 6.28, 8.94 }, { 10.2, 8.0, 20.5 } };
+//		LinkedList<Vector> rowsList = new LinkedList<Vector>();
+//		for (int i = 0; i < array.length; i++) {
+//			Vector currentRow = Vectors.dense(array[i]);
+//			rowsList.add(currentRow);
+//		}
+//		JavaRDD<Vector> rows = jsc.parallelize(rowsList);
+//
+//		// Create a RowMatrix from JavaRDD<Vector>.
+//		RowMatrix mat = new RowMatrix(rows.rdd());
+//
+//		// Compute the top 3 singular values and corresponding singular vectors.
+//		SingularValueDecomposition<RowMatrix, Matrix> svd = mat.computeSVD(3, true, 1.0E-9d);
+//		RowMatrix U = svd.U();
+//		Vector s = svd.s();
+//		Matrix V = svd.V();
+	}
+	
+	public static void test2(){
+		SparkConf conf = new SparkConf().setAppName("SVDTest").setMaster("local[2]");
+		 JavaSparkContext sc = new JavaSparkContext(conf);
+	  
+	    JavaRDD<String> data = sc.textFile("/home/yurnom/data/svd.txt");
+	    JavaRDD<Vector> rows = null;
+	 
+	    RowMatrix mat = new RowMatrix(rows.rdd());
+	    //第一个参数3意味着取top 3个奇异值，第二个参数true意味着计算矩阵U，第三个参数意味小于1.0E-9d的奇异值将被抛弃
+	    SingularValueDecomposition<RowMatrix, Matrix> svd = mat.computeSVD(3, true, 1.0E-9d);
+	    RowMatrix U = svd.U(); //矩阵U
+	    Vector s = svd.s(); //奇异值
+	    Matrix V = svd.V(); //矩阵V
+	    System.out.println(s);
+	    System.out.println("-------------------");
+	    System.out.println(V);
+	}
+}
